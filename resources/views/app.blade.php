186c821bacfdd474b541ac6fa4b420e2560deb68
Tauhid219 <rezatawhid@gmail.com>
2026-09-16 15:37:37 +0600

    feat(branding): add custom Skyline Heights vector favicon and multi-resolution icons
    - Designed modern architectural skyline SVG favicon (favicon.svg)
    - Generated multi-resolution 32x32 and 48x48 ICO file (favicon.ico)
    - Generated 192x192 PNG apple-touch-icon.png and mobile icons
    - Linked SVG, ICO, and Apple Touch icons in app.blade.php head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon & Brand Icons -->
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
